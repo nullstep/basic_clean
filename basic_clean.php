d29286279b02a8c981681cc63d3a6040094dbff8
nullstep <me@nullstep.com>
2024-07-05 08:10:32 +0100

    fix lorem shortcode

--- basic_clean.php
+++ basic_clean.php
@@ -1380,7 +1380,9 @@ function bc_lorem_shortcode($atts = [], $content = null, $tag = '') {
 	$first = 1;
 
 	for ($p = 0; $p < $count; $p++) {
-		$text .= '<p>';
+		if ($p != 0) {
+			$text .= '<p>';
+		}
 		if ($p == 0) {
 			for ($l = 0; $l < 5; $l++) {
 				$text .= (($l == 0) ? ucwords($words[$l]) : $words[$l]) . ' ';
@@ -1395,8 +1397,11 @@ function bc_lorem_shortcode($atts = [], $content = null, $tag = '') {
 			$text = rtrim($text, ', ') . '. ';
 			$first = 0;
 		}
-		$text .= '</p>';
+		if ($p < ($count - 1)) {
+			$text .= '</p>';
+		}
 	}
+
 	return $text;
 }
 
